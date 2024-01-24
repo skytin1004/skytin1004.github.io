35eda27b20aee42bb0904ef14aa1e3d947c66d94
skytin1004 <skytin1004@gmail.com>
2024-01-24 13:18:06 +0000

    update about

--- about.js
+++ about.js
@@ -1,63 +1,42 @@
-////////////////////////////////////
-// NAVIGATION SHOW/HIDE
-<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-$("nav ul").hide();
+    // SMOOTH SCROLLING WITH NAV HEIGHT OFFSET
+    var navHeight = $("nav").outerHeight();
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - navHeight
+                }, 1000);
+                return false;
+            }
+        }
+    });
 
-$(".nav-toggle").click( function() {
-  $("nav ul").slideToggle("medium");
-});
+    // NAVIGATION STICKY
+    var viewHeight = $(window).height();
+    var navigation = $('nav');
 
-$("nav ul li a, .brand a").click( function() {
-  $("nav ul").hide();
-});
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > (viewHeight - 175)) {
+            navigation.addClass('sticky');
+        } else {
+            navigation.removeClass('sticky');
+        }
+    });
 
-////////////////////////////////////
-// SMOOTH SCROLLING WITH NAV HEIGHT OFFSET
-
-$(function() {
-  var navHeight = $("nav").outerHeight();
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top - navHeight
-        }, 1000);
-        return false;
-      }
+    // MAKE THE SPLASH CONTAINER VERTICALLY CENTERED
+    function centerSplash() {
+        var navHeight = $("nav").outerHeight();
+        var splashHeight = $(".splash .container").height();
+        var remainingHeight = $(window).height() - splashHeight - navHeight;
+        $(".splash .container").css({
+            "padding-top": remainingHeight / 2,
+            "padding-bottom": remainingHeight / 2
+        });
     }
-  });
-});
 
-////////////////////////////////////
-// NAVIGATION STICKY
-
-var viewHeight = $(window).height();
-var navigation = $('nav');
-
-$(window).scroll( function() {
-  if ( $(window).scrollTop() > (viewHeight - 175) ) { //edit for nav height
-    navigation.addClass('sticky');
-  } else {
-    navigation.removeClass('sticky');
-  }
-});
-
-////////////////////////////////////////////////
-// MAKE THE SPLASH CONTAINER VERTICALLY CENTERED
-
-function centerSplash() {
-  var navHeight = $("nav").outerHeight();
-  var splashHeight = $(".splash .container").height();
-  var remainingHeight = $(window).height() - splashHeight - navHeight;
-  $(".splash .container").css({"padding-top" : remainingHeight/2, "padding-bottom" : remainingHeight/2});
-}
-
-$( document ).ready( function() {
-  centerSplash();
-});
-
-$( window ).resize( function() {
-  centerSplash();
-});
+    centerSplash();
+    $(window).resize(function() {
+        centerSplash();
+    });
