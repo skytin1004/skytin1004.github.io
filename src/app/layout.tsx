8e740da551e4cda59aebf82d6a12b6bbad6d7fc7
skytin1004 <skytin1004@gmail.com>
2025-01-07 14:22:39 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navigation from "@/components/Navigation";
 
@@ -17,6 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-X946DNBS12`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-X946DNBS12');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.className} bg-gray-900 text-white`}>
         <Navigation />
         <div className="pt-16">
